9bb499982b8cb679284f38502725c8ec22f2d5e3
Samarthjainabout <30805083+Samarthjainabout@users.noreply.github.com>
2026-08-18 16:05:08 +0530

    Remove APP with test key

--- Xforge_0808260459pm_auto/scripts/list_ports.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import json, sys
-try:
-    from serial.tools import list_ports
-except ImportError:
-    print(json.dumps({"ok": False, "error": "pyserial not installed. Run: pip install pyserial"})); sys.exit(1)
-ports = [{"device": p.device, "description": p.description, "hwid": p.hwid} for p in list_ports.comports()]
-print(json.dumps({"ok": True, "ports": ports}))
